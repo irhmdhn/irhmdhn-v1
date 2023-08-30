2c3bb5d1887a33f76888dcbd8e1b331bb5883713
irhmdhn <irama141102@gmail.com>
2023-08-30 12:47:29 +0700

    ubah layout, dan lain sebagainya

--- src/main.js
+++ src/main.js
@@ -1,46 +1,47 @@
-{
-  [
-    ["Instagram", "https://www.instagram.com/irhmdhn.me/", "bxl-instagram"],
-    [
-      "LinkedIn",
-      "https://www.linkedin.com/in/ihwannudin-rahmadhani-097776288/",
-      "bxl-linkedin",
-    ],
-    ["GitHub", "https://github.com/irhmdhn", "bxl-github"],
-    ["Email", "mailto:irhmdhn.me@gmail.com", "bx-envelope"],
-  ].map(([name, url, icon]) =>
-    $("#nav-left ul").append(
-      "<li class='group w-16 sm:w-auto'><a href=" +
-        url +
-        " target='_blank' class='w-fit sm:w-auto hover:text-cyan-600 dark:hover:text-cyan-500  sm:dark:hover:text-cyan-600 cursor-pointer flex flex-col items-center sm:flex-row-reverse mx-auto transition'><i class='bx " +
-        icon +
-        " p-2 rounded text-2xl'></i><span class='transition-all ease-in-out hidden sm:block sm:scale-0 text-md sm:absolute sm:opacity-0 sm:left-0 group-hover:opacity-100 group-hover:scale-100 group-hover:left-20 lg:group-hover:left-[5.5rem]'>" +
-        name +
-        "</span></a></li>"
-    )
-  );
-}
-{
-  [
-    ["Beranda", "home", "bx-home-alt"],
-    ["Saya", "about-me", "bx-wink-smile"],
-    ["Portofolio", "portfolio", "bx-network-chart"],
-    ["Kontak", "contact", "bx-user-pin"],
-  ].map(([name, url, icon]) =>
-    $("#nav-right ul").append(
-      "<li class='group w-16 sm:w-auto'><a href='#" +
-        url +
-        "' class='w-fit sm:w-auto hover:text-cyan-600 dark:hover:text-cyan-500  sm:dark:hover:text-cyan-500 cursor-pointer flex flex-col items-center sm:flex-row-reverse mx-auto transition'><i class='bx " +
-        icon +
-        " p-2 rounded text-2xl'></i><span class='transition-all ease-in-out hidden sm:block sm:scale-0 text-md sm:absolute sm:opacity-0 sm:right-0 group-hover:opacity-100 group-hover:scale-100 group-hover:right-20 lg:group-hover:right-[5.5rem]'>" +
-        name +
-        "</span></a></li>"
-    )
-  );
-}
-
 $("#toggle-mode").on("click", () => {
   $("html").toggleClass("dark");
-  $("#toggle-mode").toggleClass("bx-moon bx-sun");
-  $("#toggle-mode").toggleClass("rotate-180");
+  $("#toggle-mode").toggleClass("bx-moon bx-sun rotate-180");
+});
+
+$("main").scroll(function () {
+  $("section").each(function () {
+    let scroll = $("main").scrollTop();
+    let offset = $(this).offset().top;
+    let height = $(this).outerHeight();
+    let id = $(this).attr("id");
+    if (scroll > offset && scroll < offset + height) {
+      $("#nav-right li i").each(function () {
+        $(this).removeClass("border-b-4 text-cyan-600 dark:text-gray-200");
+        $("#nav-right li a[href*=" + id + "] i").addClass(
+          "border-b-4 text-cyan-600 dark:text-gray-200 dark:hover:text-gray-200 "
+        );
+      });
+    }
+  });
+});
+
+$("#tab li").click(function () {
+  const id = $(this).data("id");
+  if (!$(this).hasClass("text-cyan-600")) {
+    $("#tab li")
+      .removeClass("text-cyan-600 border-cyan-600")
+      .addClass("border-transparent");
+    $(this)
+      .addClass("text-cyan-600 border-cyan-600")
+      .removeClass("border-transparent");
+
+    $(".tab-content").hide();
+    $(`[data-content=${id}]`).fadeIn();
+  }
+});
+
+$("#open").click(function () {
+  $("#modalOverlay").show().addClass("modal-open");
+  $("#close").click(function () {
+    var modal = $("#modalOverlay");
+    modal.removeClass("modal-open");
+    setTimeout(function () {
+      modal.hide();
+    }, 200);
+  });
 });
