4aad90a6e99f22f104dced24becd232ee0276db0
irhmdhn <irama141102@gmail.com>
2023-09-04 00:55:11 +0700

    profile, bg

--- src/main.js
+++ src/main.js
@@ -1,15 +1,32 @@
+// Theme
 $("#toggle-mode").on("click", () => {
   $("html").toggleClass("dark");
   $("#toggle-mode").toggleClass("bx-moon bx-sun rotate-180");
 });
 
+// Navbar
+// $("main").scroll(function () {
+//   $("section").each(function () {
+//     let scroll = $("main").scrollTop();
+//     let offset = $(this).offset().top;
+//     let height = $(this).outerHeight();
+//     let id = $(this).attr("id");
+//     if (scroll > offset && scroll < offset + height) {
+//       $("#nav-right li i").each(function () {
+//         $(this).removeClass("border-b-4 text-cyan-600 dark:text-gray-200");
+//         $("#nav-right li a[href*=" + id + "] i").addClass(
+//           "border-b-4 text-cyan-600 dark:text-gray-200 dark:hover:text-gray-200 "
+//         );
+//       });
+//     }
+//   });
+// });
+
 $("main").scroll(function () {
   $("section").each(function () {
-    let scroll = $("main").scrollTop();
-    let offset = $(this).offset().top;
-    let height = $(this).outerHeight();
+    const sectionTop = $(this).offset().top;
     let id = $(this).attr("id");
-    if (scroll > offset && scroll < offset + height) {
+    if ($("main").scrollTop() > sectionTop) {
       $("#nav-right li i").each(function () {
         $(this).removeClass("border-b-4 text-cyan-600 dark:text-gray-200");
         $("#nav-right li a[href*=" + id + "] i").addClass(
@@ -20,27 +37,33 @@ $("main").scroll(function () {
   });
 });
 
-$("#tab li").click(function () {
+// Tab about me
+$("#tab-buttons li").click(function () {
   const id = $(this).data("id");
   if (!$(this).hasClass("text-cyan-600")) {
-    $("#tab li")
+    $("#tab-buttons li")
       .removeClass("text-cyan-600 border-cyan-600")
       .addClass("border-transparent");
     $(this)
       .addClass("text-cyan-600 border-cyan-600")
       .removeClass("border-transparent");
 
-    $(".tab-content").hide();
-    $(`[data-content=${id}]`).fadeIn();
+    $(".tab-content").fadeOut(100);
+    setTimeout(function () {
+      $(`[data-content=${id}]`).fadeIn(100);
+    }, 200);
   }
 });
 
+// Modal
 $(".open-modal").click(function () {
-  $(this).next(".modal").removeClass("hidden");
+  let dataModal = $(this).next(".modal");
+  dataModal.data("modal") == "message"
+    ? dataModal.fadeIn().css("display", "grid")
+    : dataModal.fadeIn();
+
   $(".close-modal").click(function () {
     let modal = $(this).closest(".modal");
-    setTimeout(function () {
-      modal.addClass("hidden");
-    }, 100);
+    modal.fadeOut();
   });
 });
